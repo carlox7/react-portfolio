c4d4f6800fc006110c9100a167f9a1b3822c0ba6
Carlo Santos <carlox7@gmail.com>
2017-09-14 19:30:35 -0700

    scaffold done, added nav and dashboard components

---
+++ src/component/nav-comp/index.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+
+class Nav extends Component {
+
+  render(){
+    return(
+      <div>
+        <h1>Nav Bar</h1>
+      </div>
+    );
+  }
+}
+
+export default Nav;
