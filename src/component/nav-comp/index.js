6c09b3e264140651aac3af06548e256f1b4602d7
Carlo Santos <carlox7@gmail.com>
2017-09-18 18:18:16 -0700

    save before new branch

--- src/component/nav-comp/index.js
+++ src/component/nav-comp/index.js
@@ -1,15 +1,40 @@
 import React, {Component} from 'react';
+import {Nav, Navbar, NavItem, NavDropdown, MenuItem} from 'react-bootstrap';
 
 
-class Nav extends Component {
+
+class Navigation extends Component {
 
   render(){
     return(
-      <div>
-        <h1>Nav Bar</h1>
-      </div>
+      <Navbar inverse collapseOnSelect>
+        <Navbar.Header>
+          <Navbar.Brand>
+            <a href="#">Carlo Santos</a>
+          </Navbar.Brand>
+          <Navbar.Toggle />
+        </Navbar.Header>
+        <Navbar.Collapse>
+          <Nav>
+            <NavItem eventKey={1} href="">GitHub</NavItem>
+            <NavItem eventKey={2} href="https://www.linkedin.com/in/carlo-steven-santos/">LinkedIn</NavItem>
+            <NavDropdown eventKey={3} title="Browse" id="basic-nav-dropdown">
+              <MenuItem eventKey={3.1}>About</MenuItem>
+              <MenuItem eventKey={3.2}>Projects</MenuItem>
+              <MenuItem eventKey={3.3}>Something else here</MenuItem>
+              <MenuItem divider />
+              <MenuItem eventKey={3.3}>Separated link</MenuItem>
+            </NavDropdown>
+          </Nav>
+          <Nav pullRight>
+            <NavItem eventKey={1} href="https://www.linkedin.com/in/carlo-steven-santos/">LinkedIn</NavItem>
+            <NavItem eventKey={2} href="#">Link Right</NavItem>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+
     );
   }
 }
 
-export default Nav;
+export default Navigation;
