94499af964e390e2b1d21476a80aa40cd17ad6dd
Carlo Santos <carlox7@gmail.com>
2017-10-04 21:24:07 -0700

    changed build path in server

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static('./build'));
+app.use(express.static(`${__dirname}/build`));
 
 const PORT = process.env.PORT || 3000;
 
